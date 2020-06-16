cbf6a536d7a71ac210acab99a0441ff08520cb9d
ARM <arm7mass@gmail.com>
2020-06-16 20:13:30 +0300

    eposide 5

--- pi.py
+++ pi.py
@@ -70,6 +70,9 @@ np.random.seed(0)
 
 X = [[1, 2, 3, 2.5], [2.0, 5.0, -1.0, 2.0], [-1.5, 2.7, 3.3, -0.8]]
 
+inputs = [0, 2, -1, 3.3, -2.7, 1.1, 2.2, -100]
+output = []
+
 class Layer_Dense:
     def __init__(self, n_inputs, n_neurons ):
         self.weights = 0.1*np.random.randn(n_inputs, n_neurons)
@@ -78,6 +81,12 @@ class Layer_Dense:
     def forword(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
 
+
+class Activation_ReLU:
+    def forword(self, inputs):
+        self.output = np.maximum(0, inputs)
+
+        
 
 layer1 = Layer_Dense(4, 5)
 layer2 = Layer_Dense(5, 2)
